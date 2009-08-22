704c881e70f0451bf6e3553a890a522eccb1bb8b
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-08-22 14:08:14 +0400

    Added a named scope to return raffles for update

--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -9,4 +9,7 @@ class Raffle < ActiveRecord::Base
   # Validations
   validates_presence_of :prize, :start_time, :end_time, :hashtag
 
+  # Named Scopes
+  named_scope :for_update, 
+    :conditions => ['start_time <= ? AND end_time >= ?', Time.now.utc, (Time.now - 30.minutes).utc]
 end
--- spec/models/raffle_spec.rb
+++ spec/models/raffle_spec.rb
@@ -16,4 +16,25 @@ describe Raffle do
     specify { @it.should validate_presence_of(attr) }
   end
 
+  describe 'when returning raffles for udpate' do
+    it 'should not return raffles that have not started yet' do
+      Factory :raffle, :start_time => 1.hour.from_now
+      Raffle.for_update.should be_empty
+    end
+
+    it 'should return raffles that are running now' do
+      r = Factory :raffle, :start_time => 1.minute.ago
+      Raffle.for_update.should == [r]
+    end
+
+    it 'should return raffles that ended 30 minutes ago max' do
+      r = Factory :raffle, :start_time => 1.hour.ago, :end_time => 29.minutes.ago
+      Raffle.for_update.should == [r]
+    end
+
+    it 'should not return raffles that ended more than 30 minutes ago' do
+      r = Factory :raffle, :start_time => 1.hour.ago, :end_time => 31.minutes.ago
+      Raffle.for_update.should be_blank
+    end
+  end
 end
