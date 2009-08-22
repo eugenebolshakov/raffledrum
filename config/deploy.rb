7365ea3ab77f1340703f39f7f081eb677a272cfd
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-08-22 07:40:10 +0400

    Removed the cap task since it is almost useless

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,12 +20,6 @@ task :symlink_database_yml do
        #{release_path}/config/database.yml"
 end
 
-desc "Installs and builds gems as specified in environment.rb"
-task :rake_install_and_build_gems, :roles=>:app_admin do 
-  run "cd #{release_path}; sudo rake gems:install RAILS_ENV=#{fetch :rails_env}" 
-  # run "cd #{release_path}; sudo rake gems:build RAILS_ENV=#{fetch :rails_env}"
-end
-
 namespace(:deploy) do
   desc "Restart rails phusion passenger"
   task :restart do
@@ -34,4 +28,4 @@ namespace(:deploy) do
 end
 
 after 'deploy:update_code' , 'symlink_database_yml'
-after 'deploy' , 'rake_install_and_build_gems'
+after 'deploy' , 'gems:install'
