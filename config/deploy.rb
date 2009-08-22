9947f885280f80a53417f94ef2c655848308868d
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-08-22 07:34:16 +0400

    Added a cap task to install gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,12 @@ task :symlink_database_yml do
        #{release_path}/config/database.yml"
 end
 
+desc "Installs and builds gems as specified in environment.rb"
+task :rake_install_and_build_gems, :roles=>:app_admin do 
+  run "cd #{release_path}; sudo rake gems:install RAILS_ENV=#{fetch :rails_env}" 
+  # run "cd #{release_path}; sudo rake gems:build RAILS_ENV=#{fetch :rails_env}"
+end
+
 namespace(:deploy) do
   desc "Restart rails phusion passenger"
   task :restart do
@@ -28,3 +34,4 @@ namespace(:deploy) do
 end
 
 after 'deploy:update_code' , 'symlink_database_yml'
+after 'deploy' , 'rake_install_and_build_gems'
