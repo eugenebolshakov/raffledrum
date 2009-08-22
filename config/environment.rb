28734300c823b7a7a90ed1348a3e73636e85c150
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-08-22 07:29:11 +0400

    Added dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -39,4 +39,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   config.gem 'twitter-auth', :lib => 'twitter_auth'
+  config.gem 'json'
+  config.gem 'haml'
 end
