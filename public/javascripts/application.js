8b9174246ecc8b0f0997efe95be6551f7f4b06a1
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-08-24 01:08:56 +0400

    added js to reload the page with raffles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,8 @@ $(function() {
 
   $('#raffle_start_time').datepicker()
   $('#raffle_end_time').datepicker()
+
+  if ($('.my_raffles').size != 0) {
+    setInterval(function() { window.location.reload(); }, 1000 * 10);
+  }
 });
