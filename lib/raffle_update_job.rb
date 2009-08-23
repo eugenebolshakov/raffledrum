a509d1a473150af24c7e2dd8018ace67a84980f4
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-08-23 07:36:51 +0400

    Added tests for the raffle udpate job

--- lib/raffle_update_job.rb
+++ lib/raffle_update_job.rb
@@ -3,15 +3,17 @@ class RaffleUpdateJob < Struct.new(:id)
 
   def perform
     raffle = Raffle.find(id)
-    RaffleUpdater.new(raffle).update
-    if raffle.active?
-      curr_time = Time.now
-      schedule_time = curr_time + INTERVAL >= raffle.end_time ? 
-        raffle.end_time :
-        curr_time + INTERVAL
-      Delayed::Job.enqueue(RaffleUpdateJob.new(raffle.id), 1, schedule_time)
-    elsif raffle.future?
-      Delayed::Job.enqueue(RaffleUpdateJob.new(raffle.id), 1, raffle.start_time)
+    if raffle.future?
+      Delayed::Job.enqueue(RaffleUpdateJob.new(raffle.id), 1, raffle.start_time + INTERVAL)
+    else
+      RaffleUpdater.new(raffle).update
+      if raffle.active?
+        curr_time = Time.now
+        schedule_time = curr_time + INTERVAL >= raffle.end_time ? 
+          raffle.end_time :
+          curr_time + INTERVAL
+        Delayed::Job.enqueue(RaffleUpdateJob.new(raffle.id), 1, schedule_time)
+      end
     end
   end
 end
