cd2271f2a3e1118784d95bbb4c3ecf46316eb309
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-08-24 00:16:39 +0400

    Added google analytics

--- app/controllers/my_raffles_controller.rb
+++ app/controllers/my_raffles_controller.rb
@@ -9,8 +9,8 @@ class MyRafflesController < ApplicationController
   def create
     @raffle = current_user.raffles.new(params[:raffle])
     if @raffle.save
-      Delayed::Job.enqueue(RaffleTweetJob.new(@raffle.id))
-      Delayed::Job.enqueue(RaffleUpdateJob.new(@raffle.id))
+      Delayed::Job.enqueue(RaffleTweetJob.new(@raffle.id), 2)
+      Delayed::Job.enqueue(RaffleUpdateJob.new(@raffle.id), 1)
       Delayed::Job.enqueue(RafflePickWinnerJob.new(@raffle.id), 0, @raffle.end_time)
       flash[:notice] = 'Raffle has been created'
       redirect_to my_raffles_path
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,18 @@ module ApplicationHelper
     " and " <<
     link_to("tweet ##{raffle.hashtag}", tweet_message_url("##{raffle.hashtag}"), :target => '_blank')
   end
+
+  def google_analytics
+<<GA
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-10352909-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>a
+GA
+  end
 end
