575073e723f816217540ef3bfe6339b44a42dbb1
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-09-19 18:47:54 +0400

    Removed cheesy js updater

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,8 +3,4 @@ $(function() {
 
   $('#raffle_start_time').datepicker()
   $('#raffle_end_time').datepicker()
-
-  if ($('.my_raffles').length != 0) {
-    setInterval(function() { window.location.reload(); }, 1000 * 10);
-  }
 });
